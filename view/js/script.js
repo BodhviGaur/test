6b89de047d36cd926593e4fd3258b5040a4ea96a
Bodhvi Gaur <bgaur1@jhu.edu>
2020-09-13 21:36:53 -0400

    Updated code

---
+++ view/js/script.js
@@ -0,0 +1,1 @@
+document.getElementById("button").onclick=function(){ var text = document.getElementById("input").value; var li="<li>"+text+"</li>"; document.getElementById("list").appendChild(li); }
